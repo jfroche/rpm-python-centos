9f729db59d9492c6876338e4b35199b127bd46b5
Jean-François Roche <jfroche@affinitic.be>
2014-05-26 17:50:36 +0200

    install dependencies before running rpmbuild

--- build27.sh
+++ build27.sh
@@ -5,4 +5,5 @@ if [ ! -f python-$VERSION.el6.src.rpm ]; then
     wget -q http://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/source/SRPMS/p/python-$VERSION.fc21.src.rpm -O python-$VERSION.el6.src.rpm
 fi
 rpm -i python-$VERSION.el6.src.rpm
-rpmbuild --define "dist b${BUILD_NUMBER}r${SVN_REVISION}" -ba python-opt-$VERSION.spec
+yum-builddep -y /root/rpmbuild/SPECS/python-opt-$VERSION.spec
+rpmbuild --define "dist b${BUILD_NUMBER}r${SVN_REVISION}" -ba /root/rpmbuild/SPECS/python-opt-$VERSION.spec
