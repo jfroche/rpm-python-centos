18a940ce892094a2f6d1f4ab9b0a5611fb56a1c5
Jean-Francois Roche <jfroche@affinitic.be>
2017-07-04 23:49:39 +0200

    Create directories required by rpmbuild

--- build24.sh
+++ build24.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e -x
 VERSION="2.4.6-1"
+mkdir -p ~/rpmbuild/{RPMS,SRPMS,BUILD,SOURCES,SPECS,tmp}
 spectool -R -g /root/rpmbuild/SPECS/python-opt-$VERSION.spec
 yum-builddep -y /root/rpmbuild/SPECS/python-opt-$VERSION.spec
 cd /root/rpmbuild/SPECS
